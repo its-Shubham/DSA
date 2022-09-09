c8fb7aeedcf23c95aa6118408ecb95bc51c564c5
SHUBHAM <shubhamsharmadob@gamil.com>
2022-09-09 16:29:51 +0530

    optimised matrix diagonal sum

--- TwoDArray.java
+++ TwoDArray.java
@@ -82,7 +82,33 @@ public class TwoDArray {
 
     // diagonal sum in matrix
     public static void diagonalSum(int matrix[][]) {
+        // (brute force method) -> O(n^2)
+        /*  
+        int sum = 0;
+        for (int i = 0; i <= matrix.length - 1; i++) {
+            for (int j = 0; j <= matrix[0].length - 1; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length - 1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        */
 
+        // optimised method -> O(n)
+        int sum = 0;
+        for (int i = 0; i < matrix.length - 1; i++) {
+            // primary diagonal
+            sum += matrix[i][i];
+
+            // secondary diagonal
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - 1 - i];
+            }
+        }
+
+        System.out.println("sum of the matrix is : " + sum);
     }
 
     public static void display(int matrix[][]) {
@@ -116,6 +142,7 @@ public class TwoDArray {
                 { 5, 6, 7, 8 },
                 { 9, 10, 11, 12 },
                 { 13, 14, 15, 16 } };
-        printSpiral(matrix);
+        // printSpiral(matrix);
+        diagonalSum(matrix);
     }
 }
