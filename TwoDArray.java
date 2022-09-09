0752e95e3cdd1eb6d6b6ed53c9618db2812e0f65
SHUBHAM <shubhamsharmadob@gamil.com>
2022-09-09 12:53:11 +0530

    added spiral matrix

--- TwoDArray.java
+++ TwoDArray.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class TwoDArray {
 
@@ -33,11 +32,52 @@ public class TwoDArray {
                     System.out.println("element found at (" + i + "," + j + ")");
                     return true;
                 }
-
             }
         }
         System.out.println("element not found");
         return false;
+    }
+
+    // Spiral Matrix
+    public static void printSpiral(int matrix[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            // bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startRow == endRow) {
+                    break;
+                }
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            // left
+            for (int i = endRow - 1; i >= startRow + 1; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+            }
+
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+
     }
 
     public static void display(int matrix[][]) {
@@ -50,22 +90,27 @@ public class TwoDArray {
     }
 
     public static void main(String[] args) {
-        int matrix[][] = new int[3][3];
-        Scanner sc = new Scanner(System.in);
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = sc.nextInt();
-            }
-        }
+        // int matrix[][] = new int[3][3];
+        // Scanner sc = new Scanner(System.in);
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        // for (int i = 0; i < m; i++) {
+        // for (int j = 0; j < n; j++) {
+        // matrix[i][j] = sc.nextInt();
+        // }
+        // }
         // display(matrix);
 
         // System.out.println(smallest(matrix));
 
         // System.out.println(largest(matrix));
 
-        search(matrix, 2);
-
+        // search(matrix, 2);
+        int matrix[][] = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+        printSpiral(matrix);
     }
 }
