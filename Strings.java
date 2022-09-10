72c10f94da94a3e47a165f10c30f857de1ff0109
SHUBHAM <shubhamsharmadob@gamil.com>
2022-09-10 18:19:22 +0530

    Strings-Plaindrome,getShortestPath

---
+++ Strings.java
@@ -0,0 +1,43 @@
+public class Strings {
+    // palindrome
+    public static boolean palindrome(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static float getShortestPath(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            // North
+            if (path.charAt(i) == 'N') {
+                y++;
+            }
+            // South
+            else if (path.charAt(i) == 'S') {
+                y--;
+            }
+            // East
+            else if (path.charAt(i) == 'E') {
+                x++;
+            }
+            // West
+            else if (path.charAt(i) == 'W') {
+                x--;
+            }
+        }
+        float x2 = x * x, y2 = y * y;
+        return (float) Math.sqrt(x2 + y2);
+    }
+
+    public static void main(String[] args) {
+        // for palindrome
+        // String str = "racecar";
+        // System.out.println(palindrome(str));
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
+}
