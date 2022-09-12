97110058cd6206a669e79369fbaeac63e92d1b3b
SHUBHAM <shubhamsharmadob@gamil.com>
2022-09-12 23:51:39 +0530

    compress string (with error)

--- Strings.java
+++ Strings.java
@@ -55,24 +55,41 @@ public class Strings {
         return largest;
     }
 
-    // to upper case -> capatilize first alphabet of every word in a sentence -> using StringBuilder
-    //T.C-> O(n) -> length of string
-    public static String toUpperCase(String str){
+    // to upper case -> capatilize first alphabet of every word in a sentence ->
+    // using StringBuilder
+    // T.C-> O(n) -> length of string
+    public static String toUpperCase(String str) {
         StringBuilder sb = new StringBuilder("");
-        
+
         char ch = Character.toUpperCase(str.charAt(0));
         sb.append(ch);
 
-        for(int i =1;i<str.length();i++){
-            if(str.charAt(i) == ' ' && i<str.length()-1){
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
                 sb.append(str.charAt(i));
                 i++;
                 sb.append(Character.toUpperCase(str.charAt(i)));
-            }
-            else{
+            } else {
                 sb.append(str.charAt(i));
             }
         }
+        return sb.toString();
+    }
+
+    public static String compress(String str) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (str.charAt(i) == str.charAt(i + 1) && i < str.length() - 1) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count.toString());
+            }
+        }
+
         return sb.toString();
     }
 
@@ -80,28 +97,30 @@ public class Strings {
         // for palindrome
         // String str = "racecar";
         // System.out.println(palindrome(str));
-        
+
         // for shortest path
         // String path = "WNEENESENNN";
         // System.out.println(getShortestPath(path));
-        
+
         // for substring
         // String str = "shubham";
         // System.out.println(str.substring(0, 5));
         // System.out.println(subString(str, 0, 5));
-        
+
         // for largest String Lexicographically
         // String fruits[] = { "apple", "mango", "banana" };
         // System.out.println(largestStringLexicographically(fruits));
 
         // StringBuilder sb = new StringBuilder("");
         // for(char ch ='a';ch<='z';ch++){
-        //     sb.append(ch);
+        // sb.append(ch);
         // }
         // System.out.println(sb);
 
         // uppercase of every word in a sentence
-        String str="hi, i am shubham ";
-        System.out.println(toUpperCase(str));
+        // String str="hi, i am shubham ";
+        // System.out.println(toUpperCase(str));
+        String str = "abc";
+        System.out.println(compress(str));
     }
 }
