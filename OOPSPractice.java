74b454bc19542ff7910306907f54efccec7029b4
SHUBHAM <shubhamsharmadob@gamil.com>
2022-10-01 18:17:09 +0530

    tracking OOPS and deleting test

---
+++ OOPSPractice.java
@@ -0,0 +1,29 @@
+public class OOPSPractice{
+    //question 1 
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Complex{ ///////////////////// dont know -?
+
+    int real;
+    int imag;
+    public Complex(int r, int i){
+        this.real = r;
+        this.imag = i;
+
+    }
+    void sum(int num){
+
+    }
+
+    void diff(int num){
+
+    } 
+
+    void prod(int num){
+
+    }
+}
