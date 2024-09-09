6634fcde032d3cb78cf23ef73d0a46d8e3689d09
Shubham Sharma <shubham.sharma1@hostbooks.com>
2024-09-09 15:53:34 +0530

    Added, delete First, Delete Last, Delete at Index and Iterative Search in Singly LinkedList.

--- LinkedList/LinkedListPractice.java
+++ LinkedList/LinkedListPractice.java
@@ -11,10 +11,14 @@ public class LinkedListPractice {
         ll.addFirst(31);
         ll.print();
         System.out.println();
-        System.out.println("size is -> " + ll.size);
+        System.out.println("size is -> " + ll.getSize());
         ll.add(2, 15);
         ll.print();
         System.out.println();
-        System.out.println("size is -> " + ll.size);
+        System.out.println("size is -> " + ll.getSize());
+        System.out.println(ll.deleteAtIndex(3));
+        System.out.println("Key found at "+ll.itrSearch(10));
+        System.out.println("Key found at "+ll.itrSearch(20));
+        ll.print();
     }
 }
--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -1,9 +1,9 @@
 package LinkedList;
 
 public class SinglyLinkedList {
-    public Node head;
-    public Node tail;
-    public static int size;
+    private Node head;
+    private Node tail;
+    private int size;
 
     public void addFirst(int data) {
         Node newNode = new Node(data);
@@ -60,4 +60,96 @@ public class SinglyLinkedList {
         temp.next = newNode;
 
     }
+
+    public int deleteFirst() {
+
+        if (head == null) {
+            System.out.println("Linked list is Empty");
+            return Integer.MIN_VALUE;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int deleteLast() {
+
+        if (size == 0) {
+            System.out.println("Linked list id Empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        while (prev.next.next != null) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+
+    }
+
+    public int deleteAtIndex(int index) {
+        int value;
+        int i = 0;
+
+        if (size == 0) {
+            System.out.println("Linked list id Empty");
+            return -1;
+        } else if (size - 1 == index) {
+            value = deleteLast();
+            return value;
+        } else if (index == 1 && size == 1) {
+            value = deleteFirst();
+            return value;
+        }
+        Node current = head;
+        while (i < index - 2) {
+            current = current.next;
+            i++;
+        }
+        value = current.next.data;
+        current.next = current.next.next;
+        size--;
+        return value;
+
+    }
+
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+        if (size == 0) {
+            System.out.println("Linked List is Empty");
+            return -1;
+        }
+
+        while (temp.next != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+
+        return -1;
+
+    }
+
+    public int getSize(){
+        return size;
+    }
+
 }
